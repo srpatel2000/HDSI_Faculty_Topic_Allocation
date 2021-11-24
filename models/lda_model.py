88a81ef0e69c0f9b6924e0729120e0f40d584480
srpatel2000 <49801632+srpatel2000@users.noreply.github.com>
2021-11-24 15:21:27 -0800

    added methodology 5 files

---
+++ models/lda_model.py
@@ -0,0 +1,2 @@
+# lda model
+
--- run.py
+++ run.py
@@ -1,1 +1,25 @@
-# run.py file
+import os
+import sys
+import json
+
+
+def main(targets):
+
+	if "test" in targets:
+        if not os.path.isdir('models'):
+            print("No models available.")
+            sys.exit(0)
+        
+        if not os.path.isdir('results/model_prediction'):
+            os.mkdir('results/model_prediction')
+
+        data = targets[2]
+            
+        os.system("python models/lda_model.py" + data)
+
+
+if __name__ == '__main__':
+    if not os.path.isdir('results'):
+        os.makedirs('results')
+    targets = sys.argv[1:]
+    main(targets)
