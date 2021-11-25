50a62714c8d5757f495cff48c071cca425e3efa0
srpatel2000 <49801632+srpatel2000@users.noreply.github.com>
2021-11-24 16:05:47 -0800

    fixed spacing

--- run.py
+++ run.py
@@ -8,14 +8,14 @@ def main(targets):
 	if "test" in targets:
 		if not os.path.isdir('models'):
 			print("No models available.")
-            sys.exit(0)
-        
-        if not os.path.isdir('results/model_prediction'):
-            os.mkdir('results/model_prediction')
+			sys.exit(0)
 
-        data = targets[2]
-            
-        os.system("python models/lda_model.py" + data)
+		if not os.path.isdir('results/model_prediction'):
+			os.mkdir('results/model_prediction')
+
+		data = targets[2]
+
+		os.system("python models/lda_model.py" + data)
 
 
 if __name__ == '__main__':
